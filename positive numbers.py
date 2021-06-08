1e26f137b524e3f8860717e1a7af8c939eeb3be7
SwaathiRamanathan <85328254+SwaathiRamanathan@users.noreply.github.com>
2021-06-08 12:45:40 +0530

    Created positive numbers

---
+++ positive numbers.py
@@ -0,0 +1,14 @@
+# Python program to print positive Numbers in a List 
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+print("Output List1:")
+for num in list1:
+    if num >= 0:
+       print(num, end = " \n")
+       
+print("Output List2:")
+for num in list2:
+    if num >= 0:
+       print(num, end = " \n")
